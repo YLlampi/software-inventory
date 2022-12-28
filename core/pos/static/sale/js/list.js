633cd94c97a794c41f300735efa683ae655f3d9d
EvelynLizbeth <ecusiha@unsa.edu.pe>
2022-12-28 17:14:49 -0500

    update views report, update structure ajax in functions.js , update order in structure datatable

--- core/pos/static/sale/js/list.js
+++ core/pos/static/sale/js/list.js
@@ -37,11 +37,11 @@ var sale = {
                 {"data": "total"},
                 {"data": "id"},
             ],
+            order: [[0, "desc"], [2, "desc"]],
             columnDefs: [
                 {
                     targets: [0],
                     class: 'text-center',
-                    orderable: false,
                     render: function (data, type, row) {
                         return '<a class="badge badge-secondary badge-pill pointer" rel="number">' + data + '</a>'
                     }
@@ -49,7 +49,6 @@ var sale = {
                 {
                     targets: [-2, -3, -4, -5],
                     class: 'text-center',
-                    orderable: false,
                     render: function (data, type, row) {
                         return '$' + parseFloat(data).toFixed(2);
                     }
@@ -120,6 +119,7 @@ $(function () {
     });
 
     $('#data tbody')
+        .off()
         .on('click', 'a[rel="details"]', function () {
             var tr = tblSale.cell($(this).closest('td, li')).index();
             var data = tblSale.row(tr.row).data();
